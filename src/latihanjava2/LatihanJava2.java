283856bf50f4e4113778cda7d52eadc40d292963
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-09-23 22:26:38 +0700

    First Commit

---
+++ src/latihanjava2/LatihanJava2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjava2;
+
+/**
+ *
+ * @author lutfi
+ */
+public class LatihanJava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        System.out.println("Isi variable nilai = "+nilaiInt);
+        System.out.println("Isi variable PHI = "+PHI); 
+        System.out.println("Isi variable logika = "+nilaiLogika);
+        System.out.println("Isi variable karakter = "+nilaiKarakter);
+    }
+    
+}
